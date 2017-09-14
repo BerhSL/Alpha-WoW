4e8507b00da3d99fca2bbe91390a1123f48b4ec9
brotalnia <brotalnia@gmail.com>
2017-09-15 00:38:11 +0300

    Fix uninitialized variables crash.

--- WorldServer/Properties/AssemblyInfo.cs
+++ WorldServer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WorldServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("repack by brotalnia")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("barncastle")]
 [assembly: AssemblyProduct("WorldServer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © barncastle 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
